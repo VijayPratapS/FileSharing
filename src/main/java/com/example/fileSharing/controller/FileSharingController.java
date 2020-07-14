7aa20ed9f2c132729a53ea14b200892ed77c45a4
vp780540 <vp780540@gmail.com>
2020-07-14 13:43:02 +0530

    update on project

--- src/main/java/com/example/fileSharing/controller/FileSharingController.java
+++ src/main/java/com/example/fileSharing/controller/FileSharingController.java
@@ -1,5 +1,26 @@
 package com.example.fileSharing.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.fileSharing.model.SampleFile;
+import com.example.fileSharing.repository.UserRepo;
+
+@RestController
+@RequestMapping("/")
 public class FileSharingController {
+	@Autowired
+	static private UserRepo userrep;
+
+	@PostMapping("/register")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void register(@RequestBody SampleFile user, @RequestBody SampleFile password) {
+		UserRepo.save();
+	}
 
 }
--- src/main/java/com/example/fileSharing/model/SampleFile.java
+++ src/main/java/com/example/fileSharing/model/SampleFile.java
@@ -1,5 +1,25 @@
 package com.example.fileSharing.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-public class SampleFile {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "userfile")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SampleFile{
+	        @Id
+	         private String EmailId;
+	         private String password;
+	        private String name;
+	        private String Message;
+
+	        
 }
